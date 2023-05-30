76293e485485c49cd773ea7e6495e5a46cf23ccb
Maximiliano Andres Zurita Mañan <maximiliano.zurita@gamaleasing.cl>
2023-05-30 10:48:32 -0400

    codigo agregado adicional andres

--- build/index.js
+++ build/index.js
@@ -14,11 +14,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dataServices_1 = __importDefault(require("./services/dataServices"));
+const axios_1 = __importDefault(require("axios"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log("------------------------");
+    console.log("-RUN-");
 });
 app.route("/data").get(function (req, res) {
     return __awaiter(this, void 0, void 0, function* () {
@@ -31,3 +32,35 @@ app.route("/data").get(function (req, res) {
         }
     });
 });
+//fn prueba
+app.route("/pruebaData").get((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        let data = "client_id=d097cc02-e3e3-44b8-9cd6-dc56e5b75baa&scope=https%3A%2f%2fgama.operations.dynamics.com%2f.default&grant_type=client_credentials&client_secret=-SuZH%401F%2BatjfxU68U2%3AwXR%3DJ189%3F%5D%2BG";
+        let config = {
+            method: "post",
+            maxBodyLength: Infinity,
+            url: "https://login.microsoftonline.com/b144de9c-24dd-462b-b82f-e94de9183f99/oauth2/v2.0/token",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            data: data,
+        };
+        const response = yield axios_1.default.request(config).then((response) => {
+            let config = {
+                method: 'get',
+                maxBodyLength: Infinity,
+                url: 'https://gama.operations.dynamics.com/data/GMDimAttributeFinancialTagsBI',
+                headers: {
+                    'Authorization': 'Bearer ' + response.data.access_token,
+                    'Content-Type': 'application/json'
+                }
+            };
+            axios_1.default.request(config).then((response => {
+                res.json(response.data);
+            }));
+        });
+    }
+    catch (err) {
+        console.log(err);
+    }
+}));
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import FN from "./services/dataServices";
+import axios from "axios";
 const app = express();
 app.use(express.json());
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log("------------------------");
+  console.log("-RUN-");
 });
 app.route("/data").get(async function (req, res) {
   try {
@@ -13,4 +14,78 @@ app.route("/data").get(async function (req, res) {
   } catch (error) {
     console.log(error);
   }
+});
+
+
+
+//fn prueba
+
+app.route("/pruebaData").get(async (req, res) => {
+
+  try {
+
+    let data =
+
+      "client_id=d097cc02-e3e3-44b8-9cd6-dc56e5b75baa&scope=https%3A%2f%2fgama.operations.dynamics.com%2f.default&grant_type=client_credentials&client_secret=-SuZH%401F%2BatjfxU68U2%3AwXR%3DJ189%3F%5D%2BG";
+
+
+
+
+    let config = {
+
+      method: "post",
+
+      maxBodyLength: Infinity,
+
+      url: "https://login.microsoftonline.com/b144de9c-24dd-462b-b82f-e94de9183f99/oauth2/v2.0/token",
+
+      headers: {
+
+        "Content-Type": "application/x-www-form-urlencoded",
+
+      },
+
+      data: data,
+
+    };
+
+    const response = await axios.request(config).then((response) => {
+
+        let config = {
+
+              method: 'get',
+
+              maxBodyLength: Infinity,
+
+              url: 'https://gama.operations.dynamics.com/data/GMDimAttributeFinancialTagsBI',
+
+              headers: { 
+
+                'Authorization': 'Bearer '+ response.data.access_token, 
+
+                'Content-Type': 'application/json'
+
+              }
+
+            };
+
+       
+
+            axios.request(config).then((response => {
+
+
+
+
+                res.json(response.data)
+
+            }))
+
+    } );
+
+  } catch (err) {
+
+    console.log(err);
+
+  }
+
 });
